9226594e5318b141a559a2689364e84b3a6865eb
Vadim <hotun.vadim@gmail.com>
2017-10-07 22:07:02 +0300

    Added response model.

---
+++ app/src/main/java/com/khotiun/android/fandroidvktest/rest/model/response/WallGetResponse.java
@@ -0,0 +1,10 @@
+package com.khotiun.android.fandroidvktest.rest.model.response;
+
+import com.khotiun.android.fandroidvktest.model.WallItem;
+
+/**
+ * Created by hotun on 07.10.2017.
+ */
+
+public class WallGetResponse extends Full<BaseItemResponse<WallItem>> {
+}
